f645ac928e2ca444f26383fcf97173174278e1d5
nepalismycountry2020@gmail.com <nepalismycountry2020@gmail.com>
2023-09-27 01:11:00 +0000

    second checkin

--- models/test.sql
+++ models/test.sql
@@ -1,3 +1,6 @@
 with test as
-(select * from customer where first_name='John')
-select * from test
+(select * from customer where first_name='John'),
+with test1 as
+(select * from test)
+select * from test1
+
